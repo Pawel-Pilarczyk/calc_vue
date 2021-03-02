70e24a7055d87d98d6764e4c2da52cf2146b1289
Paweł Pilarczyk <56913394+Pawel-Pilarczyk@users.noreply.github.com>
2021-03-02 17:10:46 +0100

    v0.03

--- src/App.vue
+++ src/App.vue
@@ -1,132 +1,25 @@
 <template>
   <div id="app" class='calc-container'>
-    <input type="text" name="" id="display" disabled :value="x"><br>
-    <CalcButton v-for="digit in digits" :buttonValue="digit" :key="digit" v-model="y" :value="digit"/>
+    <Calc></Calc>
   </div>
 </template>
 
 <script>
 
 
-import CalcButton from './components/button';
+import Calc from './components/Calc'
 
 export default {
   name: 'App',
   components: {
-    CalcButton
-  },
-  data() {
-    return {
-      digits:[7,8,9,'/','C',
-              4,5,6,'*','=',
-              1,2,3,'-',
-              0,'.','+'],
-      a : 0,
-      b : 0,
-      calculationSign :'',
-      outcome:'',
-      x:'',
-      y :''
-    }
-  },
-  methods: {
-    addDigit: function(digit){
-       this.x = this.x.concat(digit);
-    },
-    assignSign(){
-      this.a = parseFloat(this.x);
-      this.x = "";
-      this.calculationSign = this.y;
-    },
-    equal(){
-      this.b = parseFloat(this.x);
-      switch(this.calculationSign){
-        case "+":{
-          this.plus();
-          break;
-        }
-        case "-":{
-          this.minus();
-          break;
-        }
-        case "*":{
-          this.multiply();
-          break;
-        }
-        case "/":{
-          this.devine();
-          break;
-        }
-        default:{
-          this.outcome = parseFloat(this.a);
-        }
-      }
-      this.x = this.outcome.toString();
-    },
-    clearData(){
-      this.a = 0;
-      this.b = 0;
-      this.x = "";
-      this.y = "";
-      this.outcome = "";
-    },
-    plus(){
-      this.outcome = this.a + this.b;
-    },
-    minus(){
-      this.outcome = this.a - this.b;
-    },
-    multiply(){
-      this.outcome = this.a * this.b;
-    },
-    devine(){
-      this.outcome = this.a / this.b;
-    }
-  },
-  watch:{
-    y(){
-
-      if( this.y == "+" ||
-          this.y == "-" ||
-          this.y == "*" ||
-          this.y == "/"
-      ){
-
-        this.assignSign();
-
-      }else if(this.y == "="){
-
-        this.equal();
-
-      }else if(this.y === 'C'){
-
-        this.clearData();
-
-      }else{
-
-         if(this.x === "" && this.y === "."){
-
-           this.addDigit('0.');
-        }else if(this.x.includes('.') && this.y === "."){
-
-           this.x = this.x;
-
-         }else{
-
-            this.addDigit(this.y);
-
-         }
-      }
-
-      this.y = "";
-
-    }
+    Calc
   }
 }
 </script>
 
 <style>
 #app {
+  padding-left: 10vw;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
---
+++ src/components/Calc.vue
@@ -0,0 +1,134 @@
+<template>
+  <div id="calc" class='calc-container'>
+    <input type="text" name="" id="display" disabled :value="x"><br>
+    <CalcButton v-for="digit in digits" :buttonValue="digit" :key="digit" v-model="y"/>
+  </div>
+</template>
+
+<script>
+
+
+import CalcButton from './button';
+
+export default {
+  name: 'Calc',
+  components: {
+    CalcButton
+  },
+  data() {
+    return {
+      digits:[7,8,9,'/','C',
+              4,5,6,'*','=',
+              1,2,3,'-',
+              0,'.','+'],
+      a : 0,
+      b : 0,
+      calculationSign :'',
+      outcome:'',
+      x:'',
+      y :''
+    }
+  },
+  methods: {
+    addDigit: function(digit){
+       this.x = this.x.concat(digit);
+    },
+    assignSign(){
+      this.a = parseFloat(this.x);
+      this.x = "";
+      this.calculationSign = this.y;
+    },
+    equal(){
+      this.b = parseFloat(this.x);
+      switch(this.calculationSign){
+        case "+":{
+          this.plus();
+          break;
+        }
+        case "-":{
+          this.minus();
+          break;
+        }
+        case "*":{
+          this.multiply();
+          break;
+        }
+        case "/":{
+          this.devine();
+          break;
+        }
+        default:{
+          this.outcome = parseFloat(this.a);
+        }
+      }
+      this.x = this.outcome.toString();
+    },
+    clearData(){
+      this.a = 0;
+      this.b = 0;
+      this.x = "";
+      this.y = "";
+      this.outcome = "";
+    },
+    plus(){
+      this.outcome = this.a + this.b;
+    },
+    minus(){
+      this.outcome = this.a - this.b;
+    },
+    multiply(){
+      this.outcome = this.a * this.b;
+    },
+    devine(){
+      this.outcome = this.a / this.b;
+    }
+  },
+  watch:{
+    y(){
+
+      if( this.y == "+" ||
+          this.y == "-" ||
+          this.y == "*" ||
+          this.y == "/"
+      ){
+
+        this.assignSign();
+
+      }else if(this.y == "="){
+
+        this.equal();
+
+      }else if(this.y === 'C'){
+
+        this.clearData();
+
+      }else{
+
+         if(this.x === "" && this.y === "."){
+
+           this.addDigit('0.');
+        }else if(this.x.includes('.') && this.y === "."){
+
+           this.x = this.x;
+
+         }else{
+
+            this.addDigit(this.y);
+
+         }
+      }
+
+      this.y = "";
+
+    }
+  }
+}
+</script>
+
+<style>
+#calc {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
